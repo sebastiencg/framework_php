75a068deba57ba0c1486711667d553d85741184a
sebastien <miantamag@gmail.com>
2024-02-12 14:36:23 +0100

    seb

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,6 @@ class HomeController extends Controller
     #[Route(uri: "/", name: "app_home_index", methods: ["GET"])]
     public function index():Response
     {
-
         return $this->render("ome/index", [
             "pageTitle"=> "Welcome to /home"
         ]);
